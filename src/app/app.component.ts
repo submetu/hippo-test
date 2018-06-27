78146e76a39e963cc15f419e6b5b9c0128276605
M Subhan Naeem <sub.metu@gmail.com>
2018-06-27 16:01:08 +0400

    design updates and frontend fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SearchService} from './services/search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  public afterSearch = false;
+  constructor(private searchService: SearchService){}
+
+  ngOnInit(){
+    this.searchService.isSearched().subscribe( isSearched =>{
+      this.afterSearch = isSearched;
+    });
+  }
 }
